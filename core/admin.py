a77e794af9cdf7e629c190a823676effe61c742c
smartdna <pkverma131@gmail.com>
2017-06-22 13:02:37 +0530

    Tempalate raw.html from raw.hmtl and register new model Registration

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Credential, Verification
+from .models import Credential, Verification, Registration
 
 class VerificationAdmin(admin.ModelAdmin):
 	list_display = ('asset_code','status','scan_time','city','operator','mobile')
@@ -11,6 +11,13 @@ class VerificationAdmin(admin.ModelAdmin):
 	search_fields = ('asset_code','mobile')
 	ordering =  ('-scan_time',)
 
+class RegistrationAdmin(admin.ModelAdmin):
+	list_display = ('asset_code','scan_time','location','product_details')
+	list_filter = ('location',)
+	search_fields = ('asset_code',)
+	ordering =  ('-scan_time',)
+
 admin.site.register(Verification,VerificationAdmin)
+admin.site.register(Registration,RegistrationAdmin)
 
 # Register your models here.
